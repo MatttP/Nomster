b24971b89a5268c22d7d2cda651daf08a070acb7
Matt Parr <mattt.parr@gmail.com>
2019-09-27 20:06:52 +0000

    added place:id (individual) page

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -14,6 +14,10 @@ class PlacesController < ApplicationController
         redirect_to root_path
     end
 
+    def show
+        @place = PLace.find(params[:id])
+    end
+
     private
 
     def place_params
