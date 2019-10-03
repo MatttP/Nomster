18b84f7c7e34530ecacdc3ce10d6685039b07fc7
Matt Parr <mattt.parr@gmail.com>
2019-10-03 16:30:34 +0000

    added validatioins to place.rb

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,8 +3,8 @@ class Place < ApplicationRecord
     geocoded_by :address
     after_validation :geocode
     
-    validates :name, presence: true
-    validates :description, :address, presence: true
-
+    validates :name, presence: true, length: {minimum: 3}, uniqueness: true
+    validates :address, presence: true, length: {in: 10..70}
+    validates :description, presence: true, length: {in: 5..250}
 end
 
