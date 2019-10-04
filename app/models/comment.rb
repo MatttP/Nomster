a51ec0f131b19cf24a5566704f9b3ddb7b3af2c8
Matt Parr <mattt.parr@gmail.com>
2019-10-04 22:02:28 +0000

    syntax and spelling correction, app functions again

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,14 +1,13 @@
 class Comment < ApplicationRecord
     belongs_to :user
     belongs_to :place
-end
 
 RATINGS = {
-    'one star': '1_star'
-    'two stars': '2_star'
-    'three_stars': '3_stars'
-    'four_stars': '4_stars'
-    'five_stars': '5_stars'
-}
+    'one star': '1_star',
+    'two stars': '2_stars',
+    'three stars': '3_stars',
+    'four stars': '4_stars',
+    'five stars': '5_stars'
+  }
 end
 
