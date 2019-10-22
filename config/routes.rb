bc520940fc641639721125f79cfb6bb8ff8554ae
Matt Parr <mattt.parr@gmail.com>
2019-10-22 00:19:55 +0000

    added user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
     resources :comments, :photos, only: :create
   end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users, only: :show
+
 end
+
